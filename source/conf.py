2d41ca4ccbcfa2dda6adb159918b97666591f866
citoruspm <vpeskovskiy@citorus.ru>
2019-08-08 14:38:17 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -68,7 +68,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = None
 
 def setup(app):
     app.add_stylesheet('my_theme.css')
@@ -78,7 +78,7 @@ def setup(app):
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
